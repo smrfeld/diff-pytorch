7c965a8b987b91488e9e547f3b2008f99164ebce
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-11-15 23:09:17 -0800

    Added test for unet

--- tests/test_unet.py
+++ tests/test_unet.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 
 import torch
-from utils import ResidualBlock, DownBlock, UpBlock
+from utils import ResidualBlock, DownBlock, UpBlock, UNet
 
 def test_residual_block():
     num_channels_input = 3
@@ -65,7 +65,8 @@ def test_up_block():
 
     num_channels_input = output_tensor.shape[1]
     ub = UpBlock(
-        num_channels_input=num_channels_input,
+        num_channels_input_signal=num_channels_input,
+        num_channels_input_skip=block_depth*[num_channels_output],
         num_channels_output=num_channels_output,
         block_depth=block_depth
         )
@@ -75,3 +76,35 @@ def test_up_block():
 
     assert output.shape == torch.Size([batch_size, num_channels_output, height, width])
 
+
+def test_unet():
+    image_size = 64
+    batch_size = 8
+
+    unet = UNet(
+        image_size=image_size,
+        noise_embedding_size=32
+        )
+
+    # Print summary of model
+    # from torchsummary import summary
+    # summary(unet, input_size=[(batch_size, 3, image_size, image_size), (batch_size, 1,1,1)])
+
+    '''
+    diffusion_times = tf.random.uniform(
+        shape=(BATCH_SIZE, 1, 1, 1), minval=0.0, maxval=1.0
+    )
+    noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+    '''
+
+    noise_rates = torch.randn(batch_size, 1, 1, 1)
+    noise_variances = noise_rates**2
+
+    # Fake image
+    input_tensor = torch.randn(batch_size, 3, image_size, image_size)
+
+    # Output
+    output_tensor = unet(input_tensor, noise_variances)
+
+    # Shape
+    assert output_tensor.shape == torch.Size([batch_size,3,image_size,image_size])
--- utils/unet.py
+++ utils/unet.py
@@ -72,14 +72,13 @@ class DownBlock(torch.nn.Module):
 
 class UpBlock(torch.nn.Module):
 
-    def __init__(self, num_channels_input: int, num_channels_output: int, block_depth: int):
+    def __init__(self, num_channels_input_signal: int, num_channels_input_skip: List[int], num_channels_output: int, block_depth: int):
         super(UpBlock, self).__init__()
         self.block_depth = block_depth
         self.rbs = []
         for i in range(block_depth):
-            num_channels_in = num_channels_input if i == 0 else num_channels_output
-            # Multiply input by 2 => add skip connection
-            num_channels_in *= 2
+            num_channels_in = num_channels_input_signal if i == 0 else num_channels_output
+            num_channels_in += num_channels_input_skip[i]
             rb = ResidualBlock(num_channels_in, num_channels_output)
             self.rbs.append(rb)
         self.up = torch.nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
@@ -105,11 +104,13 @@ class UNet(torch.nn.Module):
         self.conv_change_input = torch.nn.Conv2d(in_channels=3, out_channels=self.num_channels_initial_conv, kernel_size=1)
 
         # Noise embeddings
-        self.up = torch.nn.Upsample(size=(image_size, image_size), mode='bilinear', align_corners=False)
+        self.noise_embedding_up = torch.nn.Upsample(size=(image_size, image_size), mode='nearest')
 
         # Down blocks
+        # Input size is num_channels_initial_conv from reshaping the image
+        # +1 from the noise embedding after the up layer
         self.down_blocks = [
-            DownBlock(num_channels_input=self.num_channels_initial_conv+self.noise_embedding_size, num_channels_output=32, block_depth=2),
+            DownBlock(num_channels_input=self.num_channels_initial_conv+1, num_channels_output=32, block_depth=2),
             DownBlock(num_channels_input=32, num_channels_output=64, block_depth=2),
             DownBlock(num_channels_input=64, num_channels_output=96, block_depth=2),
             ]
@@ -122,9 +123,9 @@ class UNet(torch.nn.Module):
 
         # Up blocks
         self.up_blocks = [
-            UpBlock(num_channels_input=128, num_channels_output=96, block_depth=2),
-            UpBlock(num_channels_input=96, num_channels_output=64, block_depth=2),
-            UpBlock(num_channels_input=64, num_channels_output=32, block_depth=2),
+            UpBlock(num_channels_input_signal=128, num_channels_input_skip=[96,96], num_channels_output=96, block_depth=2),
+            UpBlock(num_channels_input_signal=96, num_channels_input_skip=[64,64], num_channels_output=64, block_depth=2),
+            UpBlock(num_channels_input_signal=64, num_channels_input_skip=[32,32], num_channels_output=32, block_depth=2),
             ]
 
         # Change shape to match image, init to zeros
@@ -135,7 +136,7 @@ class UNet(torch.nn.Module):
     def forward(self, x: torch.Tensor, noise_variances: torch.Tensor) -> torch.Tensor:
         # Embedding of noise variances
         noise_embedding = sinusoidal_embedding(noise_variances, noise_embedding_size=self.noise_embedding_size)
-        noise_embedding = self.up(noise_embedding)
+        noise_embedding = self.noise_embedding_up(noise_embedding)
 
         # Change image shape
         x = self.conv_change_input(x)
