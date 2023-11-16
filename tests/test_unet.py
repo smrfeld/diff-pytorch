3620b925d8b5791d9d8b420b1e1e50778f4ea81f
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-11-15 20:56:12 -0800

    Added up and down blocks with test

--- tests/test_unet.py
+++ tests/test_unet.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 
 import torch
-from utils import ResidualBlock
+from utils import ResidualBlock, DownBlock, UpBlock
 
 def test_residual_block():
     num_channels_input = 3
@@ -22,3 +22,26 @@ def test_residual_block():
     
     # Check if the output tensor has the correct shape
     assert output.shape == torch.Size([batch_size, num_channels_output, height, width])
+
+def test_down_block():
+    num_channels_input = 3
+    num_channels_output = 6
+    block_depth = 4
+    batch_size = 16
+    height = 8
+    width = 8
+
+    db = DownBlock(
+        num_channels_input=num_channels_input,
+        num_channels_output=num_channels_output,
+        block_depth=block_depth
+        )
+    
+    # Input = (batch_size, num_channels, height, width)
+    input_tensor = torch.randn(batch_size, num_channels_input, height, width)
+    skips = []
+    output = db((input_tensor, skips))
+
+    assert output.shape == torch.Size([batch_size, num_channels_output, height // 2, width // 2])
+
+    
--- utils/unet.py
+++ utils/unet.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Tuple, List
 
 class Swish(torch.nn.Module):
     def forward(self, x):
@@ -41,3 +42,46 @@ class ResidualBlock(torch.nn.Module):
         x = self.swish(x)
         x = self.conv_2(x)
         return x + residual
+
+
+class DownBlock(torch.nn.Module):
+
+    def __init__(self, num_channels_input: int, num_channels_output: int, block_depth: int):
+        """Input shape: (batch_size, num_features, height, width)
+        Output shape: (batch_size, num_features, height // 2, width // 2)
+        """
+        super(DownBlock, self).__init__()
+        self.block_depth = block_depth
+        self.rbs = []
+        for i in range(block_depth):
+            num_channels_in = num_channels_input if i == 0 else num_channels_output
+            rb = ResidualBlock(num_channels_in, num_channels_output)
+            self.rbs.append(rb)
+        self.avg_pool = torch.nn.AvgPool2d(kernel_size=2)
+
+    def forward(self, x: Tuple[torch.Tensor, List]) -> torch.Tensor:
+        y, skips = x
+        for rb in self.rbs:
+            y = rb(y)
+            skips.append(y)
+        y = self.avg_pool(y)
+        return y
+
+
+class UpBlock(torch.nn.Module):
+
+    def __init__(self, num_channels_input: int, num_channels_output: int, block_depth: int):
+        super(UpBlock, self).__init__()
+        self.block_depth = block_depth
+        self.rb = ResidualBlock(num_channels_input, num_channels_output)
+        self.up = torch.nn.UpsamplingBilinear2d(size=2)
+
+    def forward(self, x: Tuple[torch.Tensor, List]) -> torch.Tensor:
+        y, skips = x
+        y = self.up(y)
+        for _ in range(self.block_depth):
+            # Catenate along features
+            y = torch.cat([y, skips.pop()], dim=1)
+            y = self.rb(y)
+        return y
+
