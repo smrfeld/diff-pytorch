cbce51b9c2483bbe84ae265276f73e2c8f0a7827
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-11-16 18:04:20 -0800

    Training and validation loss

--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -1,8 +1,18 @@
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, ConcatDataset
+from torch.utils.data import DataLoader, ConcatDataset, Subset
 import torchvision.transforms as transforms
+import torch
+from typing import Tuple
 
-def make_dataloader_image_folder(image_folder: str, image_size: int = 64, dataset_repetitions: int = 5, batch_size: int = 64) -> DataLoader:
+
+def make_dataloader_image_folder(
+    image_folder: str, 
+    image_size: int = 64, 
+    dataset_repetitions: int = 5, 
+    batch_size: int = 64,
+    validation_split: float = 0.2, 
+    random_seed: int = 42
+    ) -> Tuple[DataLoader,DataLoader]:
 
     transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),  # Resize images to a fixed size
@@ -16,4 +26,23 @@ def make_dataloader_image_folder(image_folder: str, image_size: int = 64, datase
     repeated_datasets = [dataset] * dataset_repetitions
     concatenated_dataset = ConcatDataset(repeated_datasets)
 
-    return DataLoader(concatenated_dataset, batch_size=batch_size, shuffle=True)
+    # Calculate the number of samples for validation
+    num_samples = len(concatenated_dataset)
+    num_val_samples = int(validation_split * num_samples)
+    num_train_samples = num_samples - num_val_samples
+
+    # Use a fixed random seed for reproducibility
+    torch.manual_seed(random_seed)
+
+    # Split the dataset into training and validation sets
+    train_indices = list(range(num_train_samples))
+    val_indices = list(range(num_train_samples, num_samples))
+
+    train_dataset = Subset(concatenated_dataset, train_indices)
+    val_dataset = Subset(concatenated_dataset, val_indices)
+
+    # Create DataLoader instances for training and validation
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader
--- utils/train.py
+++ utils/train.py
@@ -7,7 +7,7 @@ from mashumaro import DataClassDictMixin
 import torch
 from typing import Tuple
 from loguru import logger
-
+from torch.utils.data import DataLoader
 
 @dataclass
 class Conf(DataClassDictMixin):
@@ -21,20 +21,29 @@ class Conf(DataClassDictMixin):
     num_epochs: int = 10
     learning_rate: float = 0.001
     optimizer: str = "Adam"
+    random_seed: int = 42
+    validation_split: float = 0.2
+
+    def check_valid(self):
+        assert 0 <= self.validation_split <= 1, "validation_split must be between 0 and 1"
 
 
 class DiffusionModel:
 
     def __init__(self, conf: Conf):
+        conf.check_valid()
         self.conf = conf
 
-        # Make data loader
-        self.dataloader = make_dataloader_image_folder(
+        # Make data loader for train and val splits
+        self.train_loader, self.val_loader = make_dataloader_image_folder(
             image_folder=conf.image_folder, 
             image_size=conf.image_size, 
             dataset_repetitions=conf.dataset_repetitions, 
-            batch_size=conf.batch_size
+            batch_size=conf.batch_size,
+            validation_split=conf.validation_split,
+            random_seed=conf.random_seed
             )
+
 
     def train(self):
 
@@ -51,20 +60,32 @@ class DiffusionModel:
         for epoch in range(self.conf.num_epochs):
             logger.info(f"Epoch {epoch+1}/{self.conf.num_epochs}")
 
-            model.train()
-            for input_images in self.dataloader:
-                self._train_step(input_images, model, optimizer)
+            # Take a training step
+            train_loss = 0.0
+            for input_image_batch in self.train_loader:                
+                train_loss += self._train_step(input_image_batch, model, optimizer).item()
+            
+            # Compute loss from validation set
+            val_loss = 0.0
+            for input_image_batch in self.val_loader:
+                model.eval()
+                val_loss += self._compute_loss(input_image_batch, model).item()
+
+            # Average
+            train_loss /= len(self.train_loader)
+            val_loss /= len(self.val_loader)
+
+            logger.info(f"Train loss: {train_loss:.4f}, Val loss: {val_loss:.4f}")
 
 
     def denoise(self, model: torch.nn.Module, noisy_images: torch.Tensor, noise_rates: torch.Tensor, signal_rates: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        pred_noises = model(noisy_images, noise_rates**2)
+        noise_variances = noise_rates**2
+        pred_noises = model(noisy_images, noise_variances)
         pred_images = (noisy_images - noise_rates * pred_noises) / signal_rates
         return pred_noises, pred_images
 
 
-    def _train_step(self, images: torch.Tensor, model: torch.nn.Module, optimizer: torch.optim.Optimizer):
-        # Train mode
-        model.train()
+    def _compute_loss(self, images: torch.Tensor, model: torch.nn.Module) -> torch.Tensor:
 
         # Get batch size
         batch_size = images.shape[0]
@@ -84,11 +105,21 @@ class DiffusionModel:
 
         # Compute loss = MSE
         loss = torch.mean((pred_noises - noises)**2)
-        
+        return loss
+
+
+    def _train_step(self, images: torch.Tensor, model: torch.nn.Module, optimizer: torch.optim.Optimizer) -> torch.Tensor:
+        # Train mode
+        model.train()
+
+        loss = self._compute_loss(images, model)
+
         # Backpropagate
         loss.backward()
 
         # Update model parameters
         optimizer.step()
+
+        return loss
 
 
